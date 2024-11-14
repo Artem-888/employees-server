113b9a7ac01046b62551b4e4f326457e077b2f1c
Artem Markelov <artem77timoha77@gmail.com>
2024-11-14 19:19:59 +0600

    Import employee module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TagModule } from './modules/tag/tag.modules';
+import { EmployeeModule } from './modules/employee/employee.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/employees'), TagModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/employees'),
+    TagModule,
+    EmployeeModule,
+  ],
 })
 export class AppModule {}
