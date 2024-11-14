d78ccd4e73b9f2ac2f799f7f77f74ea76a024ffe
Artem Markelov <artem77timoha77@gmail.com>
2024-11-14 18:54:29 +0600

    Query methods, tag service.

--- src/modules/tag/tag.modules.ts
+++ src/modules/tag/tag.modules.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Tag, TagSchema } from './tag.schema';
+import { TagService } from './tag.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Tag.name, schema: TagSchema }])],
-  providers: [],
+  providers: [TagService],
   controllers: [],
 })
 export class TagModule {}
---
+++ src/modules/tag/tag.service.ts
@@ -0,0 +1,52 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Tag, TagDocument } from './tag.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class TagService {
+  constructor(
+    @InjectModel(Tag.name) private tagModel: Model<TagDocument>,
+  ) {}
+
+  async create(createTagDto: { name: string }) {
+    const existingTag = await this.tagModel
+      .findOne({ name: createTagDto.name })
+      .exec();
+
+    if (existingTag) {
+      return this.throwNotFound('unique');
+    }
+
+    const newTag = new this.tagModel(createTagDto);
+    await newTag.save();
+
+    return this.tagModel.find().exec();
+  }
+
+  findAll() {
+    return this.tagModel.find().exec();
+  }
+
+  async delete(id: string) {
+    const tag = await this.tagModel.findByIdAndDelete(id).exec();
+    if (!tag) {
+      return this.throwNotFound(id);
+    }
+
+    return this.tagModel.find().exec();
+  }
+
+  deleteAll() {
+    return this.tagModel.deleteMany().exec();
+  }
+
+  private throwNotFound(item: string) {
+    const message =
+      item === 'unique'
+        ? 'Tag name must be unique.'
+        : `Tag with ID ${item} not found.`;
+
+    throw new HttpException(message, HttpStatus.NOT_FOUND);
+  }
+}
